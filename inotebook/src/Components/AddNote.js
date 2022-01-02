c469edeebe01a7334f85f31a526ce5bc93c4ce7e
Irfan Farooq <irfandominate@gmail.com>
2022-01-02 11:59:57 +0530

    added update note

--- inotebook/src/Components/AddNote.js
+++ inotebook/src/Components/AddNote.js
@@ -8,6 +8,7 @@ const AddNote = () => {
     const handleClick = (e) => {
         e.preventDefault();
         addNote(note.title, note.description, note.tag);
+        setNote({ title: "", description: "", tag: "" })
     }
     const onChange = (e) => {
         setNote({ ...note, [e.target.name]: e.target.value })
@@ -19,21 +20,21 @@ const AddNote = () => {
                 <form className='my-3'>
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" onChange={onChange} />
+                        <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" value={note.title} onChange={onChange} minLength={5} required />
 
                     </div>
                     <div className="mb-3">
                         <label htmlFor="description" className="form-label">Description</label>
-                        <input type="text" name='description' className="form-control" id="description" onChange={onChange} />
+                        <input type="text" name='description' className="form-control" id="description" value={note.description} onChange={onChange} minLength={5} required />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="tag" className="form-label">Tag</label>
-                        <input type="text" name='tag' className="form-control" id="tag" onChange={onChange} />
+                        <input type="text" value={note.tag} name='tag' className="form-control" id="tag" onChange={onChange} />
                     </div>
-                    <button type="submit" className="btn btn-primary" onClick={handleClick}>Add</button>
+                    <button disabled={note.title.length < 5 || note.description.length < 5} type="submit" className="btn btn-primary" onClick={handleClick}>Add</button>
                 </form>
             </div>
-        </div>
+        </div >
     )
 }
 
---
+++ inotebook/src/Components/Login.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            <form>
+                <div className="mb-3">
+                    <label for="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label for="password" className="form-label">Password</label>
+                    <input type="password" name='password' className="form-control" id="password" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
