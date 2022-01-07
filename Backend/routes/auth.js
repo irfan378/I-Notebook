35544ecb29c03ffb589e4bc51ae69c89e650c186
Irfan Farooq <irfandominate@gmail.com>
2022-01-07 12:38:13 +0530

    added signup

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -16,16 +16,17 @@ router.post('/createuser', [
     body('password', 'Password should be at least 6 characters').isLength({ min: 5 }),
 
 ], async (req, res) => {
+    let success = false;
     // if there are errors,return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ success, errors: errors.array() });
     }
     // check wheather the user with same email exists already
     try {
         let user = await User.findOne({ email: req.body.email });
         if (user) {
-            return res.status(400).json({ error: "Sorry a user with email already exists" });
+            return res.status(400).json({ success, error: "Sorry a user with email already exists" });
         }
         const salt = await bcrypt.genSalt(10);
         const secPass = await bcrypt.hash(req.body.password, salt);
@@ -41,7 +42,8 @@ router.post('/createuser', [
             }
         }
         const authtoken = jwt.sign(data, JWT_SECRET);
-        res.json({ authtoken });
+        success = true;
+        res.json({ success, authtoken });
     }
     catch (error) {
         console.error(error.message);
--- inotebook/src/Components/Signup.js
+++ inotebook/src/Components/Signup.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+    const [credentials, setCredentials] = useState({ name: "", email: "", password: "", cpassword: "" })
+    let navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { name, email, password } = credentials;
+        const response = await fetch('http://localhost:5000/api/auth/createuser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+
+            },
+            body: JSON.stringify({ name, email, password })
+        });
+        const json = await response.json()
+        console.log(json);
+        if (json.success) {
+            // save the auth token and redirect
+            localStorage.setItem('token', json.authtoken);
+            navigate("/");
+        }
+        else {
+            alert('Invalid credentials');
+        }
+    }
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
     return (
         <div>
-            I am signUp
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="name" aria-describedby="emailHelp" onChange={onChange} name='name' />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" name='email' id="email" aria-describedby="emailHelp" onChange={onChange} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" name='password' id="password" onChange={onChange} required minLength={5} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+                    <input type="password" className="form-control" name='cpassword' id="cpassword" onChange={onChange} required minLength={5} />
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     )
 }
