9bc2f988203e5cb0ffe239d0cba31dbdfaac5f07
Irfan Farooq <irfandominate@gmail.com>
2022-01-10 12:32:47 +0530

    added dark mode

--- inotebook/src/Components/About.js
+++ inotebook/src/Components/About.js
@@ -1,10 +1,15 @@
 
 import React from 'react'
 
-export const About = () => {
+export const About = (props) => {
+    let myStyle = {
+        color: props.mode === 'dark' ? 'white' : '#48599a',
+        backgroundColor: props.mode === 'dark' ? '#48599a' : 'dark'
+    }
     return (
-        <div>
+        <div style={myStyle}>
             <h2>This is About </h2>
+            <p>This is the iNotebook app where you can store your notes on cloud</p>
         </div>
     )
 }
